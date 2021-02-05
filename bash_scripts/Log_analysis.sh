b58b62cd72788d7d5f16092f26cb69c140db26d6
evasive10 <35576241+evasive10@users.noreply.github.com>
2021-02-04 22:26:44 -0600

    Delete bash_scripts directory

--- bash_scripts/Log_analysis.sh
+++
@@ -1,2 +0,0 @@
-cat logA.txt | sed s/INCORRECT_PASSWORD/ACCESS_DENIED/ > Update1_Combined_Access_Logs.txt
-cat Update1_Combined_Access_Logs.txt | awf -F" " '{print $4, $6}' > Update2_Combined_Access_Logs.txt
--- bash_scripts/roulette_dealer_finder_by_time.sh
+++
@@ -1,3 +0,0 @@
-while read line;do
-	grep -E "$2".*"$3" | awk '{print $1, $2, $5, $6}' >> dealers_working_during_losses
-done< "$1"_Dealer_schedule
